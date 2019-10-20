4461b010e9319b41ab4aabee0f573a57f618f061
Rob J Hyndman <robjhyndman@gmail.com>
2019-10-20 20:13:17 +1100

    Ported some of tidylife to form funtsibble

---
+++ R/funtsibble.R
@@ -0,0 +1,16 @@
+#' @importFrom dplyr mutate rename select arrange
+#' @importFrom magrittr %>%
+#' @importFrom purrr map2
+#' @importFrom rlang syms
+#' @importFrom stats na.omit
+#' @importFrom tibble as_tibble
+#' @importFrom tidyr gather spread
+#' @importFrom tidyselect everything
+#' @importFrom tsibble as_tsibble
+#'
+NULL
+
+#' @export
+magrittr::`%>%`
+#' @export
+tsibble::as_tsibble
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(funtsibble)
+
+test_check("funtsibble")
