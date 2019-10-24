e4f3e7da21e0d6bfcf48ee5c57c36df5b2af116d
robjhyndman <robjhyndman@gmail.com>
2019-10-24 22:38:47 +1100

    Created new class funtsibble

--- R/tsibble.R
+++ R/tsibble.R
@@ -1,20 +1,29 @@
-#' Coerce to a tsibble object
+#' Coerce to a funtsibble object
 #'
-#' @param x Objects to be coerced to a tsibble (`tbl_ts`).
+#' @param x Objects to be coerced to a funtsibble (`fun_tbl_ts`).
 #' @param validate `TRUE` suggests to verify that each key or each combination
 #' of key variables leads to unique time indices (i.e. a valid tsibble). If you
 #' are sure that it's a valid input, specify `FALSE` to skip the checks.
 #' @param ... Other arguments passed on to individual methods.
 #'
-#' @return A tsibble object.
-#' @rdname as-tsibble
+#' @return A funtsibble object.
+#' @rdname as_funtsibble
 #' @seealso \code{\link[tsibble]{as_tsibble}}
 #'
-#' @examples
-#' # coerce demogdata object to tsibble ----
-#' as_tsibble(demography::fr.mort)
 #' @export
-as_tsibble.demogdata <- function(x, ..., validate = TRUE) {
+as_funtsibble <- function(x, key = NULL, index, regular = TRUE,
+                       validate = TRUE, .drop = TRUE, ...) {
+  stopifnot(rlang::is_logical(regular, n = 1))
+  UseMethod("as_funtsibble")
+}
+
+#' @rdname as_funtsibble
+#'
+#' @examples
+#' # coerce demogdata object to funtsibble ----
+#' as_funtsibble(demography::fr.mort)
+#' @export
+as_funtsibble.demogdata <- function(x, ..., validate = TRUE) {
   rates_included <- ("rate" %in% names(x))
   pop_included <- ("pop" %in% names(x))
   # Avoid CRAN error check by declaring variables
@@ -78,16 +87,17 @@ as_tsibble.demogdata <- function(x, ..., validate = TRUE) {
     ) %>%
     tsibble::as_tsibble(index = Year, key = c(AgeGroup, Age, Group), validate = validate) %>%
     dplyr::arrange(Group, Year, Age)
+    class(output) <- c("fun_tbl_ts", class(output))
     return(output)
 }
 
-#' @rdname as-tsibble
+#' @rdname as_funtsibble
 #'
 #' @examples
-#' # coerce demogdata object to tsibble ----
-#' as_tsibble(rainbow::ElNino_ERSST_region_1and2)
+#' # coerce fts object to funtsibble ----
+#' as_funtsibble(rainbow::ElNino_ERSST_region_1and2)
 #' @export
-as_tsibble.fts <- function(x, ..., validate = TRUE) {
+as_funtsibble.fts <- function(x, ..., validate = TRUE) {
   output <- x$y %>%
     tibble::as_tibble() %>%
     dplyr::mutate(..x = x$x) %>%
@@ -97,5 +107,16 @@ as_tsibble.fts <- function(x, ..., validate = TRUE) {
     dplyr::select(.index, ..x, ..yname, tidyselect::everything())
   colnames(output)[colnames(output)=="..x"] <- x$xname
   colnames(output)[colnames(output)=="..yname"] <- x$yname
-  tsibble::as_tsibble(output, index = .index, key = !!rlang::sym(x$xname), validate = validate)
+  output <- tsibble::as_tsibble(output, index = .index, key = !!rlang::sym(x$xname), validate = validate)
+  class(output) <- c("fun_tbl_ts", class(output))
+  output
+}
+
+
+#' @rdname as_funtsibble
+#'
+#' @export
+as_funtsibble.tsibble <- function(x, ..., validate = TRUE) {
+  class(x) <- c("fun_tbl_ts", class(x))
+  x
 }
